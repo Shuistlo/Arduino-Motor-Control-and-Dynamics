5e474f83463a3582a5ac6bc3dab52c9c082536a2
Shuistlo <aredpenguin1@yahoo.com>
2017-11-07 10:55:46 +0000

    Add files via upload

---
+++ sketch_nov07a.ino
@@ -0,0 +1,31 @@
+#include<Encoder.h>
+
+Encoder myEnc(2,3);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Basic Encoder Test:");
+ 
+}
+
+long intStartTime = 0;
+long oldPosition = -999;
+
+void loop() {
+  long currTime = millis();
+  if( (currTime - intStartTime) > 100){
+      long newPosition = myEnc.read();
+      float radPosition = newPosition/180*PI;
+      Serial.println(radPosition);
+      Serial.println(newPosition);
+      //actual code fun fun
+       Serial.println("looping");
+
+    
+      intStartTime = millis();
+      
+    
+  }
+   
+
+}
